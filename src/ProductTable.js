6fcb39d06efd150c66982ec77264e15538f942bb
JIMIN LEE <jimin1222@naver.com>
2022-12-01 12:10:12 +0900

    commit

--- src/ProductTable.js
+++ src/ProductTable.js
@@ -1,73 +1,47 @@
 import React, {Component} from 'react';
 import ProductRow from './ProductRow';
 import './App.css';
-
 class ProductTable extends Component {
     constructor(props) {
         super(props);
-        this.state = { //데이터의 원천?
+        this.state = {
             products: [
-                {
-                    name:"Samsung S23 Plus",
-                    type:"Mobile",
-                    description:"이번엔 제대로 나와 줬으면...",
-                    brand:"Samsung"
-                },
-           
-                {
-                    name:"iphone 15",
-                    type:"Mobile",
-                    description:"예쁠듯",
-                    brand:"Apple"
-                },
-           
-                {
-                    name:"Lg1",
-                    type:"Mobile",
-                    description:"몰랐음",
-                    brand:"lg"
-                },
-
-                {
-                    name:"Kanon",
-                    type:"Camera",
-                    description:"비쌀까?",
-                    brand:"캐논"
-
-                }
-
-
             ]
         }
+    } // end of constructor
+    componentDidMount() {
+        fetch('http://localhost:7090/products')
+            .then((response) => response.json())
+            .then((responseData)=>{
+                //console.log(responseData);
+                this.setState({
+                   products: responseData
+                });
+            })
+            .catch(err => console.error(err));
     }
-
     render() {
         return (
             <div>
+                <h1>{this.props.name}을(를) 위한 제품 목록</h1>
                 <table border="1">
                     <thead>
                         <tr>
                             <th>Name</th>
                             <th>Type</th>
-                            <th>brand</th>
+                            <th>Brand</th>
                             <th>description</th>
                             <th>Edit</th>
                             <th>Delete</th>
                             <th>View</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.products.map((detail,i) => <ProductRow key={i} product={detail}/>)}
-                        </tbody>
-                        
-                        
-            
-                        </table>
-                    </div>
-
-                    );
-    }
-
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.products.map((detail,i) => <ProductRow key={i} product={detail}/>)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }  // end of render
 }
-    
 export default ProductTable;
