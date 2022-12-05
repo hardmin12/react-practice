524fcc66f0038a6bbddc9690b7aa71329c852be6
JIMIN LEE <jimin1222@naver.com>
2022-12-05 12:49:17 +0900

    final commit

--- src/ProductRow.js
+++ src/ProductRow.js
@@ -1,6 +1,30 @@
 import React, {Component} from "react";
 
 class ProductRow extends Component {
+
+
+    handleDeleteClick = (id) => {
+        if(!window.confirm("정말로 삭제하시겠습니까?")) {
+        }else{
+
+      fetch('http://localhost:7090/products/' +id,
+        {
+            method: 'DELETE',
+        })
+        .then(response => {
+            console.log('삭제되었습니다.', JSON.stringify(response));
+            window.location.reload(false);
+        })
+        .catch(error => console.log('에러:', error));
+
+        }
+    }
+        
+        
+
+      
+    
+
     render() {
         return (
             <tr>
@@ -9,7 +33,7 @@ class ProductRow extends Component {
                 <td>{this.props.product.brand}</td>
                 <td>{this.props.product.description}</td>
                 <td><a href="#">수정</a></td>
-                <td><a href="#">삭제</a></td>
+                <td><a href="#" onClick={() => this.handleDeleteClick(this.props.product.id)}>삭제</a></td>
                 <td><a href="#">뷰</a></td>
             </tr>
         );
